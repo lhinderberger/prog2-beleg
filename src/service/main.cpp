17f4fa88e73ab6bd3c07b5e8b5c4926b96701c25
Lucas Hinderberger <git@lhinderberger.de>
2017-05-18 17:42:51 +0200

    Initial commit - Project Structure and CGI Hello World

---
+++ src/service/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Content-type: application/json" << endl << endl;
+    cout << "{ \"Hello\": \"World\"}" << endl;
+    return 0;
+}
