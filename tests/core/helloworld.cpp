b9aa31776f35c001752e90e13bf33c7881171dc0
Lucas Hinderberger <git@lhinderberger.de>
2017-05-31 17:56:41 +0200

    Built Tests Skeleton

---
+++ tests/core/helloworld.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+
+TEST(HelloworldTest, BasicMath) {
+    EXPECT_EQ(1+1, 2);
+    EXPECT_EQ(1+1, 1);
+}
+
---
+++ tests/webservice/helloworld.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+
+TEST(HelloworldTest, BasicMath) {
+    EXPECT_EQ(3+4, 7);
+    EXPECT_EQ(3+4, 4);
+}
+
