4309e4552ef2dff997b10f0d40b879c9fd3a382d
Lucas Hinderberger <git@lhinderberger.de>
2017-06-08 16:48:37 +0200

    Renamed sqlite test, added Test for Author persist (test failing)

---
+++ tests/core/sql-abstraction.cpp
@@ -0,0 +1,65 @@
+#include <memory>
+
+#include <gtest/gtest.h>
+#include "core/Author.h"
+#include "core/Database.h"
+#include "core/SqlConnection.h"
+#include "core/SqlPreparedStatement.h"
+
+using namespace pb2;
+
+/*
+ * Learning test to find out which error codes SQLite emits on common SQL failures.
+ */
+TEST(SQLAbstractionTest, SQLFailureCodesTest) {
+    auto connection = SqlConnection::construct(":memory:", true);
+    try {
+        SqlPreparedStatement query(connection, "SELECT * FROM tblDoesntexist");
+    }
+    catch (SqliteException & e) {
+        EXPECT_EQ(SQLITE_ERROR, e.getSqliteErrorCode());
+    }
+}
+
+/**
+ * Find out if full column names are derived correctly
+ */
+TEST(SQLAbstractionTest, FullColumnNamesTest) {
+    auto connection = SqlConnection::construct(":memory:", true); //TODO: Move to fixture
+    connection->executeSQL("CREATE TABLE test(id INT PRIMARY KEY, value TEXT); INSERT INTO test VALUES(1,'bla'),(2,'blubb');");
+
+    SqlPreparedStatement query(connection, "SELECT id, value AS valueAlias, COUNT(*), COUNT(*) AS bla FROM test as testi");
+
+    EXPECT_EQ(4, query.getColumnCount());
+    EXPECT_EQ("test.id", query.getFullColumnName(0));
+    EXPECT_EQ("test.value", query.getFullColumnName(1));
+    EXPECT_EQ(".COUNT(*)", query.getFullColumnName(2));
+    EXPECT_EQ(".bla", query.getFullColumnName(3));
+}
+
+/**
+ * Basic test to find out if SQL persist / load works as intended.
+ */
+TEST(SQLAbstractionTest, PersistAuthorTest) {
+    auto connection = SqlConnection::construct(":memory:", true); //TODO: Move to fixture
+    auto database = Database::initialize(connection);
+
+    /* Create and persist new author */
+    auto author = Author::construct(database, 123);
+    author->setFirstName("Lucas");
+    author->setLastName("Hinderberger");
+    author->persist();
+    connection->commit();
+
+    /* Query for author and ensure they're equal */
+    SqlPreparedStatement query(connection, "SELECT * FROM author WHERE id = ?");
+    query.bindInt(0, 123);
+    query.step();
+
+    auto author2 = Author::construct(database, 0); // TODO: This is a very ugly interface...
+    author2->load(query);
+
+    EXPECT_EQ(123, author2->getId());
+    EXPECT_EQ("Lucas", author2->getFirstName());
+    EXPECT_EQ("Hinderberger", author2->getLastName());
+}
