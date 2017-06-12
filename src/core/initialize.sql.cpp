c43036e031d01b9787759d68f69f099074036101
Lucas Hinderberger <git@lhinderberger.de>
2017-06-12 11:54:32 +0200

    Added Initializing SQL for Medium and MediumCopy

--- src/core/initialize.sql.cpp
+++ src/core/initialize.sql.cpp
@@ -7,5 +7,19 @@ const char * initializingSQL = R"---(
         first_name TEXT, -- Usually, this would be something like a CHAR(32), but SQLite does not have fixed-size strings
         last_name TEXT
     );
+    CREATE TABLE media(
+        ean TEXT PRIMARY KEY,
+        type TEXT,
+        format TEXT, title TEXT, subtitle TEXT, author_id INTEGER,
+        FOREIGN KEY(author_id) REFERENCES authors(id)
+    );
+    CREATE TABLE media_copies(
+        medium_ean TEXT,
+        serial_number INT,
+        deaccessioned INT, -- boolean value, but SQLite lacks a dedicated type
+        location TEXT,
+        PRIMARY KEY(medium_ean, serial_number),
+        FOREIGN KEY(medium_ean) REFERENCES media(ean)
+    );
     INSERT INTO meta(name, value) VALUES('version', '1');
 )---";
