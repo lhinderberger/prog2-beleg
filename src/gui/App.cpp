07d60d7b874191417549638f18e514f69abcb58d
Lucas Hinderberger <git@lhinderberger.de>
2017-06-24 22:04:23 +0200

    Use System Locale

--- src/gui/App.cpp
+++ src/gui/App.cpp
@@ -6,6 +6,10 @@ using namespace std;
 App::~App() = default;
 
 bool App::OnInit() {
+    /* Initialize locale */
+    wxLocale locale(wxLocale::GetSystemLanguage());
+
+    /* Initialize main frame */
     mainFrame = new MainFrame("prog2-beleg GUI", wxDefaultPosition, wxSize(1024, 768));
     mainFrame->Show(true);
     return true;
