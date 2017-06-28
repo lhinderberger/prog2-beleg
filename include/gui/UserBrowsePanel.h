db551b0d0663b8404d3896737a71c6bd2fb0e091
Lucas Hinderberger <git@lhinderberger.de>
2017-06-28 12:16:09 +0200

    Added Lendings table to UserBrowsePanel

--- include/gui/UserBrowsePanel.h
+++ include/gui/UserBrowsePanel.h
@@ -4,15 +4,26 @@
 #include <wx/wx.h>
 
 #include "core/Database.h"
+#include "LendingsSearchTable.h"
 #include "UserSearchTable.h"
 
 namespace pb2 {
     class UserBrowsePanel : public wxPanel {
     private:
-        UserSearchTable * searchTable = nullptr;
+        LendingsSearchTable * lendingsTable = nullptr;
+        UserSearchTable * userTable = nullptr;
+        wxStaticText * lendingsHeading = nullptr;
+
+        wxString getLendingsHeading(bool userSelected) const;
+
+        void evUserSelected(wxCommandEvent & event);
 
     public:
+        enum class ID { USER_TABLE = 1, LENDINGS_TABLE };
+
         UserBrowsePanel(wxWindow * parent, std::shared_ptr<Database> database);
+
+    wxDECLARE_EVENT_TABLE();
     };
 }
 
--- src/gui/UserBrowsePanel.cpp
+++ src/gui/UserBrowsePanel.cpp
@@ -3,17 +3,43 @@
 using namespace pb2;
 using namespace std;
 
+wxBEGIN_EVENT_TABLE(pb2::UserBrowsePanel, wxPanel)
+    EVT_COMMAND((int)UserBrowsePanel::ID::USER_TABLE, PB2_EVT_ST_SELECTED, UserBrowsePanel::evUserSelected)
+wxEND_EVENT_TABLE()
+
 UserBrowsePanel::UserBrowsePanel(wxWindow * parent, shared_ptr<Database> database)
-    : wxPanel(parent, wxID_ANY) {
+        : wxPanel(parent, wxID_ANY) {
     /* Create top-level box sizer */
     wxSizer * sizer = new wxBoxSizer(wxVERTICAL);
     SetSizer(sizer);
 
     /* Add User table */
-    searchTable = new UserSearchTable(this, wxID_ANY, database);
-    sizer->Add(searchTable, 0, wxEXPAND);
-    searchTable->list();
+    userTable = new UserSearchTable(this, (int)ID::USER_TABLE, database);
+    sizer->Add(userTable, 0, wxEXPAND);
+    userTable->list();
+
+    /* Add Lendings heading */
+    lendingsHeading = new wxStaticText(this, wxID_ANY, getLendingsHeading(false));
+    sizer->Add(lendingsHeading, 0, wxTOP, 15);
 
     /* Add Lendings table */
-    //TODO
+    lendingsTable = new LendingsSearchTable(this, (int)ID::LENDINGS_TABLE, database);
+    sizer->Add(lendingsTable, 0, wxEXPAND);
+}
+
+wxString UserBrowsePanel::getLendingsHeading(bool userSelected) const {
+    static wxString baseHeading =  _("Ausleihen");
+    static wxString selectUserMsg =  baseHeading + " - " + _("Bitte zuerst Nutzer auswählen!");
+
+    /* Return only heading when a user is selected, otherwise return descriptive message */
+    return userSelected ? baseHeading : selectUserMsg;
+}
+
+void UserBrowsePanel::evUserSelected(wxCommandEvent & event) {
+    /* Retrieve ID of selected user */
+    int userId = userTable->getSelectedId();
+
+    /* Update lendings heading and table */
+    lendingsHeading->SetLabelText(getLendingsHeading(userId != 0));
+    lendingsTable->setUserId(userId);
 }
