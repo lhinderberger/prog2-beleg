b69b3570d8f664c0a1442cfbdbe795722a08e94f
Lucas Hinderberger <git@lhinderberger.de>
2017-06-28 09:55:41 +0200

    Added TwoQuerySearchTable base class for simplified creation of SearchTables
    The TwoQuerySearchTable base class contains all code for managing the two
    prepared statements commonly used by DatabaseSearchTable implementations.
    Classes implementing TwoQuerySearchTable only have to provide SQL and
    column content parsing code, everything else is abstracted away.

---
+++ include/gui/TwoQuerySearchTable.h
@@ -0,0 +1,63 @@
+#ifndef PROG2_BELEG_TWO_QUERY_SEARCH_TABLE_H
+#define PROG2_BELEG_TWO_QUERY_SEARCH_TABLE_H
+
+#include "DatabaseSearchTable.h"
+#include "core/Database.h"
+#include "core/sqlite/SqlitePreparedStatement.h"
+
+namespace pb2 {
+    /**
+     * Serves as base class for common DatabaseSearchTables that have one query for
+     * listing and another one for searching.
+     * Note: prepareSearch() will bind the query string parameter as parameter 1 of your
+     * search query, unless you explicitly call "setNoBind" in the constructor.
+     */
+    class TwoQuerySearchTable : public DatabaseSearchTable {
+    protected:
+        enum class QueryType { NONE, LIST, SEARCH };
+
+
+        virtual std::string getListSQL() = 0;
+        virtual std::string getSearchSQL() = 0;
+
+        virtual void prepareSearch(const wxString & query) override;
+        virtual void prepareList() override;
+        virtual bool step() override;
+
+        /**
+         * Retrieve the currently active query or throw logic_error if there
+         * is no active query (i.e. currentQueryType is NONE)
+         */
+        std::shared_ptr<SqlitePreparedStatement> currentQuery();
+
+        /**
+         * Call setNoBind in the constructor to keep TwoQuerySearchTable from binding
+         * the query string parameter in prepareSearch().
+         */
+        inline void setNoBind(bool noBind) { this->noBind = noBind; }
+
+        inline QueryType getCurrentQueryType() const { return currentQueryType; }
+
+    public:
+        TwoQuerySearchTable(
+                wxWindow * parent, const wxString & newItemButtonLabel,
+                const std::vector<wxString> & columnHeadings,
+                std::shared_ptr<Database> database
+        );
+        virtual ~TwoQuerySearchTable() = default;
+
+        inline std::shared_ptr<Database> getDatabase() { return database; }
+
+    private:
+        bool noBind = false;
+        QueryType currentQueryType = QueryType::NONE;
+
+        std::shared_ptr<Database> database;
+        std::shared_ptr<SqlitePreparedStatement> listQuery;
+        std::shared_ptr<SqlitePreparedStatement> searchQuery;
+
+        void generalPrepare(std::shared_ptr<SqlitePreparedStatement> & query, const std::string & sql, QueryType type);
+    };
+}
+
+#endif
---
+++ src/gui/TwoQuerySearchTable.cpp
@@ -0,0 +1,50 @@
+#include "gui/TwoQuerySearchTable.h"
+
+using namespace pb2;
+using namespace std;
+
+TwoQuerySearchTable::TwoQuerySearchTable(
+        wxWindow * parent, const wxString & newItemButtonLabel,
+        const vector<wxString> & columnHeadings, shared_ptr<Database> database
+) : DatabaseSearchTable(parent, newItemButtonLabel, columnHeadings), database(database) {
+
+}
+
+
+shared_ptr<SqlitePreparedStatement> TwoQuerySearchTable::currentQuery() {
+    if (currentQueryType == QueryType::LIST)
+        return listQuery;
+    else if (currentQueryType == QueryType::SEARCH)
+        return searchQuery;
+    else
+        throw logic_error("currentQuery: No active query!");
+}
+
+void TwoQuerySearchTable::generalPrepare(shared_ptr<SqlitePreparedStatement> & query,
+                                         const string & sql, QueryType type) {
+    /* Prepare query, if necessary */
+    if (query == nullptr)
+        query = make_shared<SqlitePreparedStatement>(database->getConnection(), sql);
+
+    /* Reset and make current */
+    query->reset();
+    currentQueryType = type;
+}
+
+void TwoQuerySearchTable::prepareSearch(const wxString & query) {
+    /* Prepare query, if necessary */
+    generalPrepare(searchQuery, getSearchSQL(), QueryType::SEARCH);
+
+    /* Bind parameter */
+    if (!noBind)
+        searchQuery->bind(1, query.ToStdString());
+}
+
+void TwoQuerySearchTable::prepareList() {
+    /* Prepare query, if necessary */
+    generalPrepare(listQuery, getListSQL(), QueryType::LIST);
+}
+
+bool TwoQuerySearchTable::step() {
+    return currentQuery()->step();
+}
